0afddee2bdc3b2e2ebc7e0a70a045d3792fd5cbd
Edward Rodríguez <luisrova17@gmail.com>
2018-03-17 23:44:17 -0600

    Versión reproductor

--- reproductorTareaCorta3/app/src/main/java/com/luis/edward/reproductortareacorta3/MainActivity.java
+++ reproductorTareaCorta3/app/src/main/java/com/luis/edward/reproductortareacorta3/MainActivity.java
@@ -1,32 +1,51 @@
 package com.luis.edward.reproductortareacorta3;
 
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer mediaPlayer;
-
-
+    Integer canciones[]=new Integer[10];
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        canciones[0]=R.raw.que_es_la_vida;
+        canciones[1]=R.raw.club_cant_handle_me;
+
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),canciones[0]);
+
     }
 
     public void clickPlay(View view)
     {
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.clubcant);
+
+
         mediaPlayer.start();
+
+
+        //mediaPlayer.pa
 
     }
 
     public void clickPause(View view)
     {
         mediaPlayer.pause();
+
+    }
+
+    public void clickSig(View view)
+    {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(),canciones[1]);
+        mediaPlayer.start();
 
     }
 }
